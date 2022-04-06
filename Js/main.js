ea1aabdf6048485653dae1b1b06e02f96d929cfa
Sergio <sergio.ivan.melgarejo@gmail.com>
2022-04-06 15:45:02 -0300

    stylos

--- Js/main.js
+++ Js/main.js
@@ -1,4 +1,5 @@
 const body = document.getElementById("body");
+const characters = document.querySelectorAll("character-container");
 
 body.addEventListener("click",(e)=>{
     // console.log("hola")
@@ -12,10 +13,31 @@ const play = document.getElementById("play");
 
 play.addEventListener("click",()=>{
     body.classList.remove("home");
-    body.classList.add("seleting");
+    body.classList.add("select");
 })
 
 
 // SELETING ________________________________ 
+const game = document.getElementById("game");
+
+game.addEventListener("click",(e)=>{
+    const classes = e.target.classList.value;
+
+    if(body.classList.contains("select")){
+
+        if(classes.indexOf("character-modal") !== -1){
+            body.classList.remove("select");
+            body.classList.add("inGame");
+
+            const selectCharacter = e.target.parentNode.dataset.selectCharacter;
+            let character = [1,2,3,4]
+        }
+    }
+    if(body.classList.contains("inGame")){
+
+    }
+})
+
+
 // INGAME ________________________________ 
 // GAMEOVER ________________________________ 
